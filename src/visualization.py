a893480930198f406e50bbd5c611cc76f64310b7
Eniko Toth <enikodonatellatoth@gmail.com>
2022-04-18 18:42:17 +0200

    Adjust font settings in visualization

--- src/visualization.py
+++ src/visualization.py
@@ -2,12 +2,19 @@ import matplotlib.pyplot as plt
 
 
 def plot_result(gaps, best_etas):
+    plt.rc('font', size=12)
+    plt.rc('axes', titlesize=12)
+    plt.rc('axes', labelsize=12)
+    plt.rc('xtick', labelsize=12)
+    plt.rc('ytick', labelsize=12)
+    plt.rc('legend', fontsize=12)
+
     figure, axis = plt.subplots(1, 1, figsize=(14, 7))
     figure.suptitle('Experiment result', fontweight='bold')
 
     shift = 0.3
     for error, gap in gaps.items():
-        axis.plot(gap.keys(), gap.values(), linewidth=2, linestyle=(0, (5, 8 + shift)), label=f'prediction error = {error}')
+        axis.plot(gap.keys(), gap.values(), linewidth=3, linestyle=(0, (5, 8 + shift)), label=f'prediction error = {error}')
         axis.scatter(best_etas[error], gap[best_etas[error]], label=f'best eta for {error} error')
         shift += 0.3
 
