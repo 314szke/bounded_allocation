51adff1698e44573e9ac5b832c0767b8e7aa654e
Eniko Toth <enikodonatellatoth@gmail.com>
2022-06-05 10:57:25 +0200

    Add new use case

--- src/manual_input.py
+++ src/manual_input.py
@@ -57,8 +57,41 @@ def Input_2():
     new_input.items.append(Item(3, 10, [2]))
     return new_input
 
+def Input_3():
+    config = ProblemConfiguration(5, 5, 100, 100, 1, 5, 100, 100, 5141)
+    new_input = ProblemInput(config)
+
+    new_input.buyers.append(Buyer(0, 100))
+    new_input.buyers[0].wanted_item_ids = [0]
+    new_input.buyers[0].potential_expense = 100
+
+    new_input.buyers.append(Buyer(1, 100))
+    new_input.buyers[1].wanted_item_ids = [0, 1]
+    new_input.buyers[1].potential_expense = 200
+
+    new_input.buyers.append(Buyer(2, 100))
+    new_input.buyers[2].wanted_item_ids = [0, 1, 2]
+    new_input.buyers[2].potential_expense = 300
+
+    new_input.buyers.append(Buyer(3, 100))
+    new_input.buyers[3].wanted_item_ids = [0, 1, 2, 3]
+    new_input.buyers[3].potential_expense = 400
+
+    new_input.buyers.append(Buyer(4, 100))
+    new_input.buyers[4].wanted_item_ids = [0, 1, 2, 3, 4]
+    new_input.buyers[4].potential_expense = 500
+
+    new_input.items.append(Item(0, 100, [0, 1, 2, 3, 4]))
+    new_input.items.append(Item(1, 100, [1, 2, 3, 4]))
+    new_input.items.append(Item(2, 100, [2, 3, 4]))
+    new_input.items.append(Item(3, 100, [3, 4]))
+    new_input.items.append(Item(4, 100, [4]))
+
+    return new_input
+
 
 MANUAL_INPUTS = {
     1: Input_1(),
-    2: Input_2()
+    2: Input_2(),
+    3: Input_3()
 }
